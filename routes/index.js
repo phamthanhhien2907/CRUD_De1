db16791da3e1dff5fb2a046973527c01063a2338
Hai Nam <haiinamm1408@gmail.com>
2022-11-09 18:19:10 +0700

    up src

--- routes/index.js
+++ routes/index.js
@@ -1,71 +1,63 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://Khoa21donga:Khoa21dongaKhoa21dongaKhoa21donga@cluster0.o9ivn6p.mongodb.net/test2?retryWrites=true&w=majority",
-  {
+var mongoose = require('mongoose');
+//Connection mongoodb
+mongoose.connect("mongodb+srv://Khoa21donga:Khoa21dongaKhoa21dongaKhoa21donga@cluster0.o9ivn6p.mongodb.net/test2?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
-let user3Schema = mongoose.Schema({
-  IdDistrict: {
-    type: String,
-  },
-  IdVillage: {
-    type: String,
-  },
-  Address: {
-    type: String,
-  },
-  IsGetNotification: {
-    type: String,
-  },
-  IsVerified: {
-    type: String,
-  },
-  IsGetOpen: {
-    type: String,
-  },
-});
-let User = mongoose.model("User", user3Schema);
+    useFindAndModify: false
+})
+
+//Schema
+let custommerSchema = mongoose.Schema({
+    phone: {
+        type: Number,
+    },
+    password: {
+        type: String,
+    },
+    register: {
+        type: String,
+    },
+    fname: {
+        type: String,
+    },
+    type: {
+        type: Number,
+    },
+})
+
+let CustommerTwo = mongoose.model('CustommerTwo', custommerSchema)
+
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  User.find({}, (Error, data) => {
-    res.render("index", { user3s: data });
-  });
+router.get('/', function(req, res, next) {
+    CustommerTwo.find({}, (error, data) => {
+            console.log(data)
+            res.render('index', { custommertwos: data })
+        })
+        // res.render('index', { title: 'Lê Hồng Phương' });
 });
-//from-add
-router.get("/form-add", function (req, res, next) {
-  res.render("form-add", {});
-});
-
-router.post("/add", function (req, res, next) {
-  User.create(req.body);
-  res.redirect("/");
-});
-
-//  form-update
-router.get("/form-update/:id", function (req, res, next) {
-  User.findById(req.params.id, (error, data) => {
-    res.render("form-update", { lop: data });
-  });
-});
-// req.body.id update
-router.post("/update", function (req, res, next) {
-  console.log("Quant", req.body);
-  User.findByIdAndUpdate(req.body.id, req.body, (Error, data) => {
-    res.redirect("/");
-  });
-});
-
-//lệnh
-
-router.get("/form-delete/:id", function (req, res, next) {
-  User.findByIdAndDelete(req.params.id, (Error, data) => {
-    res.redirect("/");
-  });
-});
-
+router.get('/form-add', function(req, res, next) {
+    res.render('form-add', {})
+})
+router.post('/add', function(req, res, next) {
+    CustommerTwo.create(req.body);
+    res.redirect('/')
+})
+router.get('/form-update/:id', function(req, res, next) {
+    CustommerTwo.findById(req.params.id, (error, data) => {
+        res.render('form-update', { custommertwo: data })
+    })
+})
+router.post('/update', function(req, res, next) {
+    CustommerTwo.findByIdAndUpdate(req.body.id, req.body, (error, data) => {
+        res.redirect('/')
+    })
+})
+router.get('/form-delete/:id', function(req, res, next) {
+    CustommerTwo.findByIdAndDelete(req.params.id, (error, data) => {
+        res.redirect('/')
+    })
+})
 module.exports = router;
